562fba7b3db3582d8e69d4e6b0746e45a5b93ea6
kroneldo28 <kroneldo28@yahoo.fr>
2022-03-27 23:52:24 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import turtle
+import pandas
+
+
+screen = turtle.Screen()
+screen.title("U.S States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+states_data = pandas.read_csv("50_states.csv")
+states = states_data.state
+
+correct_guesses = []
+# We loop until we have all the states found
+while len(correct_guesses) < 50:
+    user_answer = screen.textinput(prompt="Guess a State", title=f"{len(correct_guesses)}/50 correct guesses").title()
+    if user_answer == "Exit":
+        break
+    if user_answer in states.values:
+        print(user_answer)
+        turtle.penup()
+        state_dataframe = states_data[states_data.state == user_answer]
+        x_cor = float(state_dataframe.x)
+        y_cor = float(state_dataframe.y)
+        state_text = turtle.Turtle()
+        state_text.hideturtle()
+        state_text.penup()
+        state_text.goto(x_cor, y_cor)
+        state_text.write(f"{user_answer}")
+        correct_guesses.append(user_answer)
+
+# Let's save the missing states to a csv
+list_of_states = states_data.state.to_list()
+for state in correct_guesses:
+    if state in list_of_states:
+        list_of_states.remove(state)
+
+missing_states = pandas.Series(data=list_of_states)
+
+missing_states.to_csv("missing_states.csv")
+
+
